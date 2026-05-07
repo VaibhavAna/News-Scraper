1124310230427f546144aa15bd14539b857439c1
Vaibhav <vaibhavanand2494@gmail.com>
2026-05-07 16:07:14 +0530

    Story.js

---
+++ mern-hn-scraper/backend/src/models/User.js
@@ -0,0 +1,39 @@
+//In models we always stored user schema.
+//Here we have use 3 fields name,email,password,bookmarks
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    bookmarks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Story",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);

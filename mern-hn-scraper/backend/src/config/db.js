ad287e3d29fc27e6e04576eb107ba5ff349fb05a
Vaibhav Anand <vaibhavanand2494@gmail.com>
2026-05-08 19:44:15 +0530

    allupdated

--- mern-hn-scraper/backend/src/config/db.js
+++ mern-hn-scraper/backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://codewithvaibhav:Demo123@ac-3y2bipn-shard-00-00.0hreltm.mongodb.net:27017,ac-3y2bipn-shard-00-01.0hreltm.mongodb.net:27017,ac-3y2bipn-shard-00-02.0hreltm.mongodb.net:27017/?ssl=true&replicaSet=atlas-waj6ra-shard-0&authSource=admin&appName=News");
 
     console.log("MongoDB Connected");
   } catch (error) {
